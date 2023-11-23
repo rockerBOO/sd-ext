7f54341a87593d3926a2096045b53ae4a907c131
rockerBOO <rockerboo@gmail.com>
2023-11-23 02:33:47 -0500

    add argument for read-metadata

--- read-metadata.py
+++ read-metadata.py
@@ -1,5 +1,19 @@
-from safetensors import safe_open
-path = "/mnt/900/builds/stable-diffusion-webui/extensions/sd-webui-additional-networks/models/lora/boo.safetensors"
+import argparse
+import json
 
-with safe_open(path, "pt") as file:
-    print(file.metadata())
+from safetensors import safe_open
+
+
+def main(args):
+    with safe_open(args.path, "pt") as file:
+        parsed = file.metadata()
+        print(json.dumps(parsed, indent=4))
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "path", help="Path to the safetensors file to read the metadata"
+    )
+    args = argparser.parse_args()
+    main(args)
