6a40f02ee5b11dec5019338fd24bb73292b6b8d6
rockerBOO <rockerboo@gmail.com>
2025-01-15 13:37:01 -0500

    Update read-tensors for better output

--- read-tensors.py
+++ read-tensors.py
@@ -5,30 +5,31 @@ from safetensors import safe_open
 
 
 def main(args):
-    with safe_open(args.path, "pt") as file:
-        for i, key in enumerate(file.keys()):
-            t = file.get_tensor(key)
-            # print(key, t.size(), t.norm())
-            print(key)
+    file = safe_open(args.path, "pt")
 
-            if t.dtype == torch.int64:
-                if len(t.shape) == 1 and t.shape[0] == 1:
-                    print(t.size(), 'items', t.item())
-                else:
-                    print(t.size(), 'tensor', t)
-            elif len(t.shape) == 0:
-                print("No tensor")
-                continue
+    print(args.path)
+
+    for i, key in enumerate(file.keys()):
+        t = file.get_tensor(key)
+        print(key)
+
+        if t.dtype == torch.int64:
+            if len(t.shape) == 1 and t.shape[0] == 1:
+                print(t.dtype, t.size(), "items", t.item())
             else:
-                print(t.size())
-                print(t.size(), 'norm', t.norm().item())
+                print(t.dtype, t.size(), "tensor", t)
 
+        elif len(t.shape) == 0:
+            print("No tensor")
+            continue
+        else:
+            print(t.dtype, t.size(), "norm", t.norm().item())
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
-        "path", help="Path to the safetensors file to read the metadata"
+        "path", help="Path to the safetensors file to read the tensors"
     )
     args = argparser.parse_args()
     main(args)
